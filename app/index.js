6a23c9c1f54fbe8a5d0ad25f7a32a0699638cbee
Joshua Michalik <smileyfacepancakes@gmail.com>
2015-04-11 11:09:06 -0400

    Add prompts

--- app/index.js
+++ app/index.js
@@ -5,7 +5,28 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 
 var YodyGenerator = yeoman.generators.Base.extend({
+    var promptUser: function() {
+        var done = this.async();
 
+        console.log(this.yeoman);
+
+        var prompts = [{
+            name: 'appName',
+            message: 'What is your app\'s name ?'
+        },{
+            type: 'confirm',
+            name: 'addDemoSection',
+            message: 'Would you like to generate a demo section ?',
+            default: true
+        }];
+
+        this.prompt(prompts, function (props) {
+            this.appName = props.appName;
+            this.appDemoSection = props.addDemoSection;
+
+            done();
+        }.bind(this));
+    }
 });
 
 module.exports = YodyGenerator;
